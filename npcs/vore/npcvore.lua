358cf52cfaec77f523e5cbe57e70a99c5e321860
Sheights <Sheights@hotmail.com>
2015-09-21 15:25:24 -0400

    Super Alpha NPC vore
    many bugs, alot of oversight, features not fully implemented, but it
    "works"

--- npcs/vore/npcvore.lua
+++ npcs/vore/npcvore.lua
@@ -1,60 +1,77 @@
 local oldInit = init
 local oldUpdate = update
+local stopWatch = 0
+local fed = false
+local victim = nil
 
 function init()
 	oldInit()
-	
-	world.logInfo( "I am alive" )
-	local fed = false
+
 --	local person = personalityType()		if we want to use a special fed personality
-	local victim = nil
 	
-	message.setHandler("eaten", function(_, _, params)
-		fed = true
+	entity.setItemSlot( "head", "wolfcostumehead" )
+	entity.setItemSlot( "chest", "wolfcostumechest" )
+	entity.setItemSlot( "legs", "wolfcostumelegs" )
+	
+--	message.setHandler("eaten", function(_, _, params)
+--		fed = true
 --		entity.setItemSlot( "chest", "wolfcostumebelly")
 --		setPersonality("fed")
-	end)
+--	end)
 	
-	message.setHandler("letgo", function(_, _, params)
-		fed = false
-		victim = nil
+--	message.setHandler("letgo", function(_, _, params)
+--		fed = false
+--		victim = nil
 --		entity.setItemSlot( "chest", "wolfcostumebelly")
 --		setPersonality("person")
-	end)
+--	end)
 end
 
-function search()
-
---	local people = world.entityQuery( entity.toAbsolutePosition({ 0, 0 }), 20, {
---     includedTypes = {"npc"},
---     boundMode = "CollisionArea"
- --   })
+function feed()
+	people = world.entityQuery( mcontroller.position(), 10, {
+		includedTypes = {"npc"},
+		boundMode = "CollisionArea"
+	})
 	
---	if #people == 2 then
---		world.logInfo( " has found someone to eat." )
+	if #people == 2 then
 		
---		if people[1] == entity.id() then
---			victim = people[2]
---		else
---			victim = people[1]
---		end
---		world.spawnProjectile( "npcvoreprojectile" , world.entityPosition( victim ), entity.id(), { 0, 0 }, false )
---		world.logInfo( " has attempted to eat "  )
-		entity.setItemSlot( "chest", wolfbellychest )
-		entity.setItemSlot( "legs", wolfbellylegs )
---		chest = entity.getItemSlot( "chest" )
---		legs = entity.getItemSlot( "legs" )
+		if people[1] == entity.id() then
+			victim = people[2]
+		else
+			victim = people[1]
+		end
 		
---		world.logInfo( tostring(chest) )
---		world.logInfo( tostring(legs) )
---		fed = true
---	end
+		world.spawnProjectile( "npcvoreprojectile" , world.entityPosition( victim ), entity.id(), { 0, 0 }, false )
+		
+		entity.setItemSlot( "chest", "wolfbellychest" )
+		entity.setItemSlot( "legs", "wolfbellylegs" )
+
+		fed = true
+	end
+end
+
+function digest()
+	
+	if stopWatch > 50 then
+		entity.setItemSlot( "chest", "wolfcostumechest" )
+		entity.setItemSlot( "legs", "wolfcostumelegs" )
+		fed = false
+		stopWatch = 0
+	end
+	
 end
 
 function update(dt)
+	tempUpdate = update
 	oldUpdate(dt)
+
 	
-	if not fed then
-		search()
+	if not fed and math.random(1000) == 1 then
+		feed()
+	elseif fed then
+		stopWatch = stopWatch + 1
+		digest()
 	end
+	
+	update = tempUpdate
 end
