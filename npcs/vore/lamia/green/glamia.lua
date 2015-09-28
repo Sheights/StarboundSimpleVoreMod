e79d3b625cac7ae1e7f8ecbd98aab301cce41516
Sheights <Sheights@hotmail.com>
2015-09-27 21:02:37 -0400

    Super Content Update!
    Lamias, Slime Girls, Avali, Poster, Sergal, Rheyya, and Yoko.

---
+++ npcs/vore/lamia/green/glamia.lua
@@ -0,0 +1,5 @@
+require "/scripts/vore/npcvore.lua"
+
+legs = "greenlamia"
+
+fulllegs = "greenlamiabelly"
---
+++ npcs/vore/slime/green/gslime.lua
@@ -0,0 +1,5 @@
+require "/scripts/vore/npcvore.lua"
+
+legs = "slimegreenlegs"
+
+fulllegs = "slimegreenlegsbelly"
