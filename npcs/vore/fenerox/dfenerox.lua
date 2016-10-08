1a88f404cac65326bd24b69b25ba626db516102f
Sheights <Sheights@hotmail.com>
2016-10-08 16:29:33 -0400

    8.3.0
    Patreon Rewards:
    
    - Sign Updated
    - New Figures
      - Charizard
      - Katojana
      - Sparky the Chu
    
    NPCs:
    
    - Fenerox
      - Added tokens and costumes for Fenerox
    
    Other:
    
    - Meta data added, so it looks better in your mod list.

---
+++ npcs/vore/fenerox/dfenerox.lua
@@ -0,0 +1,41 @@
+require "/scripts/vore/npcvore.lua"
+
+playerLines = {	"Squirming prey. Please continue. Warm and good!",
+				"Large belly. So full. Heavy and content~",
+				"Tasty friend. Wet and trapped. Gleeful and happy!",
+				"Eating fun! Will do again. So delicious and sweet!",
+				"Full stomach. Almost bursting. Gurgly and groaning~"
+}
+
+isDigest = true
+
+function initHook()
+
+	index = npc.getItemSlot("legs").parameters.colorIndex
+	
+	legs = {
+		name = "feneroxlegs",
+		parameters = {
+					colorIndex = index
+	}}
+	
+	fulllegs = {
+		name = "feneroxlegsbelly",
+		parameters = {
+					colorIndex = index
+	}}
+
+end
+
+function loseHook()
+
+	isPlayer = false
+
+end
+
+function updateHook()
+{
+	if isPlayer and math.random(700) == 1 then
+		npc.say( playerLines[math.random(#playerLines)])
+	end
+}
